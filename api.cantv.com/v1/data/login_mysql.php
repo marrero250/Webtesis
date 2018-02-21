3bbffbed3a9626ac7f18b01b5b4410d6cb645cac
Joseph <marrero250@gmail.com>
2018-02-21 14:29:13 -0400

    modificaciones en el Api
    Modificaciones en el api

--- api.cantv.com/v1/data/login_mysql.php
+++ api.cantv.com/v1/data/login_mysql.php
@@ -4,9 +4,9 @@
  * Mysql.
  */
 // TODO: cambiar por dominio o IP en producción
-define("MYSQL_HOST", "localhost");
-define("MYSQL_DATABASE_NAME", "bdcantv-usuarios");
+define("MYSQL_HOST", "us-cdbr-iron-east-05.cleardb.net");
+define("MYSQL_DATABASE_NAME", "heroku_2aaa395d60b13db");
 // TODO: cambiar en producción
-define("MYSQL_USERNAME", "jmarre03");
+define("MYSQL_USERNAME", "bb4bb3ca5bf31c");
 // TODO: cambiar en producción
-define("MYSQL_PASSWORD", "marrero250");
+define("MYSQL_PASSWORD", "0e52b6b9");
